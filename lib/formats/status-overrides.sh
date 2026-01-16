e1b118bc7b68a72e1b2cb61abc4be4d806c47dad
lucifer-morningstar <slobeck@gmail.com>
2026-01-16 13:51:09 -0800

    Generic commit

--- lib/formats/status-overrides.sh
+++ lib/formats/status-overrides.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 LOCAL_ROOT="$( tmux show -gqv @LOCAL_ROOT )"
 source "$LOCAL_ROOT/lib/share/dump.fun"
-dump ">> status overrides called"
+dump ">> status overrides"
 tmux set -g status-justify centre
-
+tmux set -g status-style bg=#{@Dark0_H},fg=#{@Light0_S}
