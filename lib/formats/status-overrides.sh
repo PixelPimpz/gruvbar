833861b6eb4c3c31939589e1b2a1d0cc7e64a1e0
lucifer-morningstar <slobeck@gmail.com>
2026-01-17 11:25:55 -0800

    Generic commit

--- lib/formats/status-overrides.sh
+++ lib/formats/status-overrides.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# ------------------------------------------------------------
+# | these overrides are for general status-bar settings ONLY |
+# | any styles or formats for components (units) should be   | 
+# | kept with the compunents themselves                      |
+# ------------------------------------------------------------
 LOCAL_ROOT="$( tmux show -gqv @LOCAL_ROOT )"
 source "$LOCAL_ROOT/lib/share/dump.fun"
 dump ">> status overrides"
@@ -7,4 +12,3 @@ tmux set -g status-style bg=#{default},fg=#{@Light0_S}
 tmux set -g window-status-style fg=#{@Dark4}
 tmux set -g window-status-current-style fg=#{@Orange}
 tmux set -g window-status-last-style fg=#{@Yellow}
-
