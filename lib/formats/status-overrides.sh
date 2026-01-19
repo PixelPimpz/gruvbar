3258af3dff24c3d8f6477b28a47477363ba18e66
lucifer-morningstar <slobeck@gmail.com>
2026-01-19 12:02:01 -0800

    Generic commit

--- lib/formats/status-overrides.sh
+++ lib/formats/status-overrides.sh
@@ -16,3 +16,5 @@ tmux set -g window-status-activity-style bg=#{default},fg=#{default}
 tmux set -g status-right-length 0
 tmux set -g message-style "#[bg=#{@Dark0},fg=#{@Orange},align=centre]#{@Alert}"
 tmux set -g message-command-style "#[bg=#{@Dark0},fg=#{@Orange},align=centre]#{@Alert}"
+tmux set -g 
+
