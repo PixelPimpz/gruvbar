50686ffcbd275f86ea3fb173e10de0567412a077
lucifer-morningstar <slobeck@gmail.com>
2026-01-19 12:12:43 -0800

    Generic commit

--- lib/formats/status-overrides.sh
+++ lib/formats/status-overrides.sh
@@ -7,6 +7,7 @@
 LOCAL_ROOT="$( tmux show -gqv @LOCAL_ROOT )"
 source "$LOCAL_ROOT/lib/share/dump.fun"
 dump ">> status overrides"
+tmux set -g @message-tmux-reload "#[fg=#{@DarkYellow},bg=#{@Dark0},align=centre]#{@Alert}#[fg=#{@Yellow}]  ~/.tmux.conf reloaded  #[fg=#{@Darkyellow}]#{@Alert}"
 tmux set -g status-justify centre
 tmux set -g status-style bg=#{default},fg=#{@Light0_S}
 tmux set -g window-status-style fg=#{@Dark4}
@@ -16,5 +17,3 @@ tmux set -g window-status-activity-style bg=#{default},fg=#{default}
 tmux set -g status-right-length 0
 tmux set -g message-style "#[bg=#{@Dark0},fg=#{@Orange},align=centre]#{@Alert}"
 tmux set -g message-command-style "#[bg=#{@Dark0},fg=#{@Orange},align=centre]#{@Alert}"
-tmux set -g 
-
