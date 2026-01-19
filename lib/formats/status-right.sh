26ede38c50720759b8d2aa11d630d9e9d8621305
lucifer-morningstar <slobeck@gmail.com>
2026-01-18 20:19:01 -0800

    Generic commit

--- lib/formats/status-right.sh
+++ lib/formats/status-right.sh
@@ -13,7 +13,7 @@ main()
   ## read the units and place them in their respective
   #  places
   tmux set -ag status-right "#{E:@nvim-info-unit}"
-  tmux set -ag status-right "#{T:@clockette}"
+  tmux set -ag status-right "#{T:@clockette}#{T:@date}"
   ## Data dumps for testing/debugging
   dump ">> SHARE: $SHARE"
 }
