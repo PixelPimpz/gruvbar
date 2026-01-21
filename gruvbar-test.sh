c78d2ecb13519e63b9be37033e1dc412457dac75
lucifer-morningstar <slobeck@gmail.com>
2026-01-21 09:51:32 -0800

    Generic commit

--- gruvbar-test.sh
+++ gruvbar-test.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
+ICONS="$( tmux show -gqv @ICONS )"
 SHARE="$( tmux show -gqv @CHER )"
 source "$SHARE/dump.fun"
 source "$SHARE/fatal.fun"
 source "$SHARE/yaml2arr.fun"
 
 dump ">> gruvbox-test.sh running..."
+yq eval ".gruvbox_colors.*.*" "./lib/gruvbox.yaml"
