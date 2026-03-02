dfbe6d0f787b77a0ce8a688687f7c174121fbcda
lucifer-morningstar <slobeck@gmail.com>
2026-03-01 20:59:06 -0800

    Generic commit

--- formats/status-overrides.sh
+++ formats/status-overrides.sh
@@ -8,7 +8,7 @@ tmux set -g destroy-unattached true
 tmux set -g status-justify centre
 tmux set -g status-style "#[bg=default,fg=#{@fg3}]"
 tmux set -g window-status-style fg="#{@bg3}"
-tmux set -g window-status-current-style "#[fg=#{@yellow_b},bold]"
+tmux set -g window-status-current-style fg="#{@yellow_b},bold"
 tmux set -g window-status-last-style fg="#{@fg4}"
 tmux set -g window-status-activity-style fg="#{default}"
 tmux set -g status-right-length 0
